b8573d9a8f1a1364005ef3034498ac620b80d79a
Tommaso Padovan <tommaso.pado@gmail.com>
2018-03-21 16:39:37 +0100

    fix everithing + add door

--- RoomEscape/RoomEscape/Source/RoomEscape/PostionReporter.cpp
+++ RoomEscape/RoomEscape/Source/RoomEscape/PostionReporter.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
-
+#include "PostionReporter.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UPostionReporter::UPostionReporter()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -15,17 +15,20 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UPostionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString name = GetOwner()->GetName();
+	FVector pos = GetOwner()->GetActorTransform().GetLocation();
+	FString posStr = pos.ToCompactString();
+	UE_LOG(LogTemp, Warning, TEXT("In coordinate %s c'e' un dio %s di mona che sbatte le porte"), *posStr, *name);
 	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UPostionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
--- RoomEscape/RoomEscape/Source/RoomEscape/PostionReporter.h
+++ RoomEscape/RoomEscape/Source/RoomEscape/PostionReporter.h
@@ -4,17 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "PositionReport.generated.h"
+#include "PostionReporter.generated.h"
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class ROOMESCAPE_API UPositionReport : public UActorComponent
+class ROOMESCAPE_API UPostionReporter : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
-	UPositionReport();
+	UPostionReporter();
 
 protected:
 	// Called when the game starts
